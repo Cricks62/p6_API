3a1c86c17a81d7c5cdfb5da6e0a92e95096d1f37
clement <derochclement@gmail.com>
2023-01-04 12:32:41 +0100

    base de donées creer avec MongoDB et relier a l'app.js création du model sauce

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const ModelSauce = require('./models/ModelSauce');
+
+
+mongoose.connect('mongodb+srv://Clement:cergy95800@clusters.zpluytu.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 const app = express();
 
-app.use((req, res) => {
-    res.json ({message : 'votre message'});
-});
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
 
 module.exports = app;
